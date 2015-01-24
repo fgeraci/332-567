bb8d26221edf4d4d0f85d475575d8cf7f9ca1539
fgeraci <fernando.geraci@rutgers.edu>
2015-01-24 15:39:08 -0500

    Second push with built and responding version running on local tomcat 8 instance with jdk 8 - 1.5 compliant

---
+++ src/main/java/edu/rutgers/components/MainController.java
@@ -0,0 +1,15 @@
+package edu.rutgers.components;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MainController {
+	
+	@ResponseBody
+	@RequestMapping(value = "/")
+	public String helloWorld() {
+		return "Hello, world!";
+	}
+}
