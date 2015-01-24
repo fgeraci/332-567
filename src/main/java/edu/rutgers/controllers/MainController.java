6dee0e9c8d4ced0b446d69a73a4fba14bb8340fb
fgeraci <fernando.geraci@rutgers.edu>
2015-01-24 15:40:39 -0500

    Stable framework

--- src/main/java/edu/rutgers/controllers/MainController.java
+++ src/main/java/edu/rutgers/controllers/MainController.java
@@ -1,4 +1,4 @@
-package edu.rutgers.components;
+package edu.rutgers.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
